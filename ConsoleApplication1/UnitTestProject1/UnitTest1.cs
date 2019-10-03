ec08c9d9e404f86ce1682bb3445fab227d25f3d8
Hưng Võ <vominhhung5102000@gmail.com>
2019-10-03 11:43:43 +0700

    Exercise04

--- ConsoleApplication1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ConsoleApplication1;
+using ConsoleApplication2;
 
-
-namespace UnitTestProject1
+namespace UnitTestProject2
 {
     [TestClass]
     public class UnitTest1
@@ -14,7 +13,7 @@ namespace UnitTestProject1
             int result1 = Program.Ceil((float)2.3);
             Assert.AreEqual(3, result1);
 
-            int result2 = Program.Ceil(-2.3f);
+            int result2 = Program.Ceil((float)-2.3f);
             Assert.AreEqual(-2, result2);
 
             int result3 = Program.Ceil(3);
@@ -26,11 +25,48 @@ namespace UnitTestProject1
             int result1 = Program.Floor((float)2.3);
             Assert.AreEqual(2, result1);
 
-            int result2 = Program.Floor((float)-3.4f);
-            Assert.AreEqual(-4, result2);
+            int result2 = Program.Floor((float)-2.3f);
+            Assert.AreEqual(-3, result2);
 
             int result3 = Program.Floor(3);
             Assert.AreEqual(3, result3);
+        }
+        [TestMethod]
+        public void pow()
+        {
+            int result2 = Program.pow(2, 3);
+            Assert.AreEqual(8, result2);
+
+            int result3 = Program.pow(3, 0);
+            Assert.AreEqual(1, result3);
+
+            int resultAm7 = Program.pow(2, -7);
+            Assert.AreEqual(0, resultAm7);
+        }
+        [TestMethod]
+        public void Abs()
+        {
+            int resultAm2 = Program.Abs(-2);
+            Assert.AreEqual(2, resultAm2);
+
+            int result3 = Program.Abs(3);
+            Assert.AreEqual(3, result3);
+
+            int result0 = Program.Abs(0);
+            Assert.AreEqual(0, result0);
+        }
+        [TestMethod]
+        public void Factorial()
+        {
+            long result2 = Program.Factorial(2);
+            Assert.AreEqual(2, result2);
+
+            long result3 = Program.Factorial(3);
+            Assert.AreEqual(6, result3);
+
+            long result0 = Program.Factorial(0);
+            Assert.AreEqual(1, result0);
+
         }
     }
 }
